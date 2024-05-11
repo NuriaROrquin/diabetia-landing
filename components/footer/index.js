a2b0f74572a2331faddc807adce7c062da2dc587
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2024-05-11 03:04:39 -0300

    using nextjs elements

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
 
@@ -5,20 +8,20 @@ export const Footer = () => {
             <div className="container mx-auto px-8 flex justify-between items-center py-8">
                     <div className="flex flex-col">
                             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                                <a href="#"
-                                   className="no-underline text-white hover:text-gray-900 transition-all">Quienes somos</a>
+                                <Link href="#"
+                                   className="no-underline text-white hover:text-gray-900 transition-all">Quienes somos</Link>
                             </li>
                             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                                <a href="#"
-                                   className="no-underline text-white hover:text-gray-900 transition-all">Quienes somos</a>
+                                <Link href="#"
+                                   className="no-underline text-white hover:text-gray-900 transition-all">Quienes somos</Link>
                             </li>
                             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                                <a href="#"
-                                   className="no-underline text-white hover:text-gray-900 transition-all">Nuestra historia</a>
+                                <Link href="#"
+                                   className="no-underline text-white hover:text-gray-900 transition-all">Nuestra historia</Link>
                             </li>
                     </div>
                     <div>
-                        <img src="/logo-blanco.png" className="w-20" alt="logo diabetIA" />
+                        <Image src="/logo-blanco.png" width="80" height="80" alt="logo diabetIA" />
                     </div>
             </div>
         </footer>
--- components/navigation/index.js
+++ components/navigation/index.js
@@ -1,22 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export const Navigation = () => {
     return (
         <nav id="header" className="fixed w-full z-30 top-0 text-white">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                 <div className="pl-4 flex items-center">
-                    <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+                    <Link className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
                        href="/">
-                        <img src="/logo-blanco.png" className="w-12" alt="logo diabetIA" />
-                    </a>
+                        <Image src="/logo-blanco.png" width={48} height={48} alt="logo diabetIA" />
+                    </Link>
                 </div>
                 <div
                     className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
                     id="nav-content">
                     <ul className="list-reset lg:flex justify-end flex-1 items-center">
                         <li className="mr-3">
-                            <a className="inline-block text-white no-underline hover:font-bold transition-all ease-in-out py-2 px-4" href="#">Conocenos</a>
+                            <Link className="inline-block text-white no-underline hover:font-bold transition-all ease-in-out py-2 px-4" href="#">Conocenos</Link>
                         </li>
                         <li className="mr-3">
-                            <a className="inline-block text-white no-underline hover:font-bold transition-all ease-in-out py-2 px-4" href="#">Sumate</a>
+                            <Link className="inline-block text-white no-underline hover:font-bold transition-all ease-in-out py-2 px-4" href="#">Sumate</Link>
                         </li>
                     </ul>
                 </div>
