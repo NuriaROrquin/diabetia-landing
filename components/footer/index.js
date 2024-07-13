49150007a933ffed4c1c585c615c50261e6f0d24
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-07-13 00:01:08 -0300

    fix white text

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,25 +3,25 @@ import Image from "next/image";
 
 export const Footer = () => {
     return (
-        <footer className="bg-blue-secondary">
+        <footer className="bg-blue-secondary text-white">
             <div className="container mx-auto px-8 flex justify-between items-center py-8">
                 <div className="flex flex-col">
-                    <p className="font-bold mb-4 text-blue-primary">Seguinos en nuestras redes:</p>
+                    <p className="font-bold mb-4">Seguinos en nuestras redes:</p>
                     <ul className="flex flex-col space-y-2">
                         <li className="inline-block mr-2 md:block md:mr-0">
-                            <Link href="https://www.instagram.com/diabetia.app" target="_blank" className="flex items-center no-underline text-blue-primary hover:font-bold transition-all">
+                            <Link href="https://www.instagram.com/diabetia.app" target="_blank" className="flex items-center no-underline hover:font-bold transition-all">
                                 <i className="fab fa-instagram mr-2"></i>
                                 Instagram
                             </Link>
                         </li>
                         <li className="inline-block mr-2 md:block md:mr-0">
-                            <Link href="https://www.youtube.com/@diabetia" target="_blank" className="flex items-center no-underline text-blue-primary hover:font-bold transition-all">
+                            <Link href="https://www.youtube.com/@diabetia" target="_blank" className="flex items-center no-underline hover:font-bold transition-all">
                                 <i className="fab fa-youtube mr-2"></i>
                                 YouTube
                             </Link>
                         </li>
                         <li className="inline-block mr-2 md:block md:mr-0">
-                            <Link href="mailto:app.diabetia@gmail.com" className="flex items-center no-underline text-blue-primary hover:font-bold transition-all">
+                            <Link href="mailto:app.diabetia@gmail.com" className="flex items-center no-underline hover:font-bold transition-all">
                                 <i className="fas fa-envelope mr-2"></i>
                                 Correo
                             </Link>
